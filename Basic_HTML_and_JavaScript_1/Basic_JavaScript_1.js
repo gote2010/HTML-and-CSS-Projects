998eabdc64a421cd1901149d1e4745df6f92340d
gote2010 <nsuhlavert@yahoo.com>
2025-09-13 05:13:13 +0100

    Add One-Page Website project

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,14 @@
+<!DOCTYPEhtml>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <title>JavaScript Alert method</title>
+<script> 
+alert("Welcome! This is my first JavaScript alert method.")
+</script>
+</head>
+<body>
+     <h1>Basic HTML with JavaScript</h1>
+ function My_First_Function() {var string="Kiss me, I'm Irish!"; var result= string.foncolor("green"); document.getElementByld("Irish").innerHTML=result;}
+</body>
+</html>
